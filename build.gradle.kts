68d820a7c9deeddeb690a305b71bb1e9ac7a4bd5
Alejandro Mujica <aledrums@gmail.com>
2024-05-13 09:10:14 -0400

    Created base libs project

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.5"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.qaroni"
+version = "0.0.1"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
